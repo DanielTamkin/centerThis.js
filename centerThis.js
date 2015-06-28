f9d27ecd1d872562d0de0110050c474a09aaa0a1
DanielTamkin <dudearoo@gmail.com>
2015-06-28 11:11:37 -0500

    Updated readme.md, added source file.

---
+++ centerThis.js
@@ -0,0 +1,51 @@
+/*centerThis.js - by Danieltamkin*/
+/*center an element to its parent.*/
+(function( $ ) {
+	$.fn.centerThis = function(data) {
+		$data = data;
+		$this = $(this);
+		function calculateTop(){
+			if($data.toLowerCase() == "browser"){
+				var browserHeight = $(window).height(),
+						elementHeight	= $this.height(),
+						elementTop	 	= (browserHeight/2) - elementHeight;
+			}
+			else{
+				var browserHeight = $this.parent().height(),
+						elementHeight	= $this.height(),
+						elementTop	 	= (browserHeight/2) - elementHeight;
+			}
+			console.log("calculateTop: "+elementTop);
+			return elementTop;
+		}
+		function calculateLeft(){
+			if($data.toLowerCase() == "browser"){
+				var browserWidth 	= $(window).width(),
+						elementWidth 	= $this.width(),
+						elementLeft	 	= (browserWidth/2) - (elementWidth/2);
+						console.log("calculateLeft: "+elementLeft);
+			}
+			else{
+				var browserWidth 	= $this.parent().width(),
+						elementWidth 	= $this.width(),
+						elementLeft	 	= (browserWidth/2) - (elementWidth/2);
+			}
+
+			return elementLeft;
+		}
+		function calculate(){
+			var elementTop 	= calculateTop(),
+					elementLeft = calculateLeft();
+			console.log(elementTop+": :"+elementLeft);
+			$this.css("position","absolute");
+			$this.css("top",elementTop+"px");
+			$this.css("left",elementLeft+"px");
+		}
+		return this.each(function() {
+			calculate();
+			$(window).resize(function() {
+				calculate();
+			});
+		});
+	 };
+}( jQuery ));
