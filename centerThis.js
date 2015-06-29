b08c402b32f8e240e564547b412e4219497014f4
DanielTamkin <dudearoo@gmail.com>
2015-06-29 11:04:45 -0500

    Fixed Resize to parent math.

--- centerThis.js
+++ centerThis.js
@@ -2,18 +2,23 @@
 /*center an element to its parent.*/
 (function( $ ) {
 	$.fn.centerThis = function(data) {
-		$data = data;
+		if(data == undefined){
+			$data = "nothing";
+		}
+		else{
+			$data = data;
+		}
 		$this = $(this);
 		function calculateTop(){
 			if($data.toLowerCase() == "browser"){
 				var browserHeight = $(window).height(),
 						elementHeight	= $this.height(),
-						elementTop	 	= (browserHeight/2) - elementHeight;
+						elementTop	 	= (browserHeight/2) - (elementHeight/2);
 			}
 			else{
 				var browserHeight = $this.parent().height(),
 						elementHeight	= $this.height(),
-						elementTop	 	= (browserHeight/2) - elementHeight;
+						elementTop	 	= (browserHeight/2) - (elementHeight/2);
 			}
 			console.log("calculateTop: "+elementTop);
 			return elementTop;
